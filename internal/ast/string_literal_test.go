896c8e29be843e88f17113f18e7657008bd6430d
Connor van Spronssen <info@dobefu.com>
2025-08-10 15:32:13 +0200

    test: Add tests for the StringLiteral struct

---
+++ internal/ast/string_literal_test.go
@@ -0,0 +1,29 @@
+package ast
+
+import "testing"
+
+func TestStringLiteral(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		input         ExprNode
+		expectedValue string
+		expectedPos   int
+	}{
+		{
+			input:         &StringLiteral{Value: "test", Pos: 0},
+			expectedValue: "test",
+			expectedPos:   0,
+		},
+	}
+
+	for _, test := range tests {
+		if test.input.Expr() != test.expectedValue {
+			t.Errorf("expected '%s', got '%s'", test.expectedValue, test.input.Expr())
+		}
+
+		if test.input.Position() != test.expectedPos {
+			t.Errorf("expected pos '%d', got '%d'", test.expectedPos, test.input.Position())
+		}
+	}
+}
