2014a87b5baa52c99f7e04489cbbd86759f7fb80
Connor van Spronssen <info@dobefu.com>
2025-08-20 17:57:21 +0200

    test: Add tests for the assignment statement

---
+++ internal/ast/assignment_statement_test.go
@@ -0,0 +1,49 @@
+package ast
+
+import "testing"
+
+func TestAssignmentStatement(t *testing.T) {
+	t.Parallel()
+
+	assignment := &AssignmentStatement{
+		Left: &Identifier{
+			Value:    "x",
+			StartPos: 0,
+			EndPos:   1,
+		},
+		Right: &NumberLiteral{
+			Value:    "1",
+			StartPos: 0,
+			EndPos:   1,
+		},
+		StartPos: 0,
+		EndPos:   1,
+	}
+
+	visitedNodes := []string{}
+	expectedNodes := []string{"x = 1", "x", "x", "1", "1"}
+
+	assignment.Walk(func(node ExprNode) bool {
+		visitedNodes = append(visitedNodes, node.Expr())
+
+		return true
+	})
+
+	if len(visitedNodes) != 5 {
+		t.Fatalf("Expected 5 visited nodes, got %d", len(visitedNodes))
+	}
+
+	for idx, node := range visitedNodes {
+		if node != expectedNodes[idx] {
+			t.Fatalf("Expected \"%s\", got \"%s\"", expectedNodes[idx], node)
+		}
+	}
+
+	if assignment.StartPosition() != 0 {
+		t.Fatalf("Expected start position to be 0, got %d", assignment.StartPosition())
+	}
+
+	if assignment.EndPosition() != 1 {
+		t.Fatalf("Expected end position to be 1, got %d", assignment.EndPosition())
+	}
+}
