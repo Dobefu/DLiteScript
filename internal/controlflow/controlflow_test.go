784b3028d8c6dc952d6f1a5a2ec964630c0c01ea
Connor van Spronssen <info@dobefu.com>
2025-08-22 09:44:27 +0200

    test: Add tests for the controlflow package

---
+++ internal/controlflow/controlflow_test.go
@@ -0,0 +1,33 @@
+package controlflow
+
+import (
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/datavalue"
+)
+
+func TestControlFlow(t *testing.T) {
+	t.Parallel()
+
+	regularResult := NewRegularResult(datavalue.Number(1))
+
+	if !regularResult.IsNormalResult() {
+		t.Errorf("Expected normal result, got %v", regularResult)
+	}
+
+	if regularResult.Value != datavalue.Number(1) {
+		t.Errorf("Expected regular result, got %v", regularResult)
+	}
+
+	breakResult := NewBreakResult(1)
+
+	if !breakResult.IsBreakResult() {
+		t.Errorf("Expected break result, got %v", breakResult)
+	}
+
+	continueResult := NewContinueResult(1)
+
+	if !continueResult.IsContinueResult() {
+		t.Errorf("Expected continue result, got %v", continueResult)
+	}
+}
