9d9ad8a915a7d74bdf5a512417bed416d99f87a5
Connor van Spronssen <info@dobefu.com>
2025-08-23 11:06:21 +0200

    test: Add tests for break and continue statement evaluation

---
+++ internal/evaluator/evaluate_break_statement_test.go
@@ -0,0 +1,51 @@
+package evaluator
+
+import (
+	"io"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/ast"
+	"github.com/Dobefu/DLiteScript/internal/controlflow"
+)
+
+func TestEvaluateBreakStatement(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		input    *ast.BreakStatement
+		expected *controlflow.EvaluationResult
+	}{
+		{
+			input: &ast.BreakStatement{
+				Count:    0,
+				StartPos: 0,
+				EndPos:   1,
+			},
+			expected: controlflow.NewBreakResult(0),
+		},
+		{
+			input: &ast.BreakStatement{
+				Count:    1,
+				StartPos: 0,
+				EndPos:   1,
+			},
+			expected: controlflow.NewBreakResult(1),
+		},
+	}
+
+	for _, test := range tests {
+		result, err := NewEvaluator(io.Discard).evaluateBreakStatement(test.input)
+
+		if err != nil {
+			t.Fatalf("error evaluating %s: %s", test.input.Expr(), err)
+		}
+
+		if !result.IsBreakResult() {
+			t.Fatalf("expected break result, got \"%v\"", result.Control.Type)
+		}
+
+		if result.Control.Count != test.expected.Control.Count {
+			t.Fatalf("expected %d, got %d", test.expected.Control.Count, result.Control.Count)
+		}
+	}
+}
---
+++ internal/evaluator/evaluate_continue_statement_test.go
@@ -0,0 +1,51 @@
+package evaluator
+
+import (
+	"io"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/ast"
+	"github.com/Dobefu/DLiteScript/internal/controlflow"
+)
+
+func TestEvaluateContinueStatement(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		input    *ast.ContinueStatement
+		expected *controlflow.EvaluationResult
+	}{
+		{
+			input: &ast.ContinueStatement{
+				Count:    0,
+				StartPos: 0,
+				EndPos:   1,
+			},
+			expected: controlflow.NewContinueResult(0),
+		},
+		{
+			input: &ast.ContinueStatement{
+				Count:    1,
+				StartPos: 0,
+				EndPos:   1,
+			},
+			expected: controlflow.NewContinueResult(1),
+		},
+	}
+
+	for _, test := range tests {
+		result, err := NewEvaluator(io.Discard).evaluateContinueStatement(test.input)
+
+		if err != nil {
+			t.Fatalf("error evaluating %s: %s", test.input.Expr(), err)
+		}
+
+		if !result.IsContinueResult() {
+			t.Fatalf("expected continue result, got \"%v\"", result.Control.Type)
+		}
+
+		if result.Control.Count != test.expected.Control.Count {
+			t.Fatalf("expected %d, got %d", test.expected.Control.Count, result.Control.Count)
+		}
+	}
+}
