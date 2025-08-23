6de2bd5c13ba08e2247bd47f3c50ba5ebfdefa08
Connor van Spronssen <info@dobefu.com>
2025-08-23 13:03:24 +0200

    test: Add tests for the RequestID struct

---
+++ internal/jsonrpc2/request_id_test.go
@@ -0,0 +1,55 @@
+package jsonrpc2
+
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+)
+
+func TestRequestID(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		input    *RequestID
+		expected string
+	}{
+		{
+			input:    &RequestID{value: json.RawMessage("1")},
+			expected: "1",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.input.String(), func(t *testing.T) {
+			t.Parallel()
+
+			if test.input.String() != test.expected {
+				t.Errorf("expected \"%v\", got \"%v\"", test.expected, test.input.String())
+			}
+
+			if test.input.IsNull() {
+				t.Errorf("expected \"%v\", got \"%v\"", false, test.input.IsNull())
+			}
+
+			jsonBytes, err := test.input.MarshalJSON()
+
+			if err != nil {
+				t.Errorf("expected no error, got \"%v\"", err)
+			}
+
+			if !bytes.Equal(jsonBytes, test.input.value) {
+				t.Errorf("expected \"%v\", got \"%v\"", test.input.value, jsonBytes)
+			}
+
+			err = test.input.UnmarshalJSON(jsonBytes)
+
+			if err != nil {
+				t.Errorf("expected no error, got \"%v\"", err)
+			}
+
+			if !bytes.Equal(test.input.value, jsonBytes) {
+				t.Errorf("expected \"%v\", got \"%v\"", test.input.value, jsonBytes)
+			}
+		})
+	}
+}
