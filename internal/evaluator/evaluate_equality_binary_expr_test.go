2f773e183eaa5051ba5f07a2a4f74af808377a1c
Connor van Spronssen <info@dobefu.com>
2025-08-23 11:24:58 +0200

    test: Add tests for the EvaluateEqualityBinaryExpr method

---
+++ internal/evaluator/evaluate_equality_binary_expr_test.go
@@ -0,0 +1,102 @@
+package evaluator
+
+import (
+	"io"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/ast"
+	"github.com/Dobefu/DLiteScript/internal/datavalue"
+	"github.com/Dobefu/DLiteScript/internal/token"
+)
+
+func TestEvaluateEqualityBinaryExpr(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		inputLeft  datavalue.Value
+		inputRight datavalue.Value
+		inputNode  *ast.BinaryExpr
+		expected   datavalue.Value
+	}{
+		{
+			inputLeft:  datavalue.Number(5),
+			inputRight: datavalue.Number(5),
+			inputNode: &ast.BinaryExpr{
+				Left:  &ast.NumberLiteral{Value: "5", StartPos: 0, EndPos: 1},
+				Right: &ast.NumberLiteral{Value: "5", StartPos: 2, EndPos: 3},
+				Operator: token.Token{
+					Atom:      "==",
+					TokenType: token.TokenTypeEqual,
+					StartPos:  0,
+					EndPos:    0,
+				},
+				StartPos: 0,
+				EndPos:   3,
+			},
+			expected: datavalue.Bool(true),
+		},
+		{
+			inputLeft:  datavalue.String("5"),
+			inputRight: datavalue.String("5"),
+			inputNode: &ast.BinaryExpr{
+				Left:  &ast.StringLiteral{Value: "5", StartPos: 0, EndPos: 1},
+				Right: &ast.StringLiteral{Value: "5", StartPos: 2, EndPos: 3},
+				Operator: token.Token{
+					Atom:      "==",
+					TokenType: token.TokenTypeEqual,
+					StartPos:  0,
+					EndPos:    0,
+				},
+				StartPos: 0,
+				EndPos:   3,
+			},
+			expected: datavalue.Bool(true),
+		},
+		{
+			inputLeft:  datavalue.Bool(true),
+			inputRight: datavalue.Bool(true),
+			inputNode: &ast.BinaryExpr{
+				Left:  &ast.BoolLiteral{Value: "true", StartPos: 0, EndPos: 1},
+				Right: &ast.BoolLiteral{Value: "true", StartPos: 2, EndPos: 3},
+				Operator: token.Token{
+					Atom:      "==",
+					TokenType: token.TokenTypeEqual,
+					StartPos:  0,
+					EndPos:    0,
+				},
+				StartPos: 0,
+				EndPos:   3,
+			},
+			expected: datavalue.Bool(true),
+		},
+		{
+			inputLeft:  datavalue.Null(),
+			inputRight: datavalue.Null(),
+			inputNode: &ast.BinaryExpr{
+				Left:  &ast.NullLiteral{StartPos: 0, EndPos: 1},
+				Right: &ast.NullLiteral{StartPos: 2, EndPos: 3},
+				Operator: token.Token{
+					Atom:      "==",
+					TokenType: token.TokenTypeEqual,
+					StartPos:  0,
+					EndPos:    0,
+				},
+				StartPos: 0,
+				EndPos:   3,
+			},
+			expected: datavalue.Bool(true),
+		},
+	}
+
+	for _, test := range tests {
+		result, err := NewEvaluator(io.Discard).evaluateEqualityBinaryExpr(test.inputLeft, test.inputRight, test.inputNode)
+
+		if err != nil {
+			t.Fatalf("error evaluating %s: %s", test.inputNode.Expr(), err)
+		}
+
+		if result.Value.DataType().AsString() != test.expected.DataType().AsString() {
+			t.Fatalf("expected \"%v\", got \"%v\"", test.expected.DataType().AsString(), result.Value.DataType().AsString())
+		}
+	}
+}
