04a799b196ce068d83b395957e9dfb883a24eccc
Connor van Spronssen <info@dobefu.com>
2025-08-23 10:28:59 +0200

    test: Add tests for the EvaluateBlockStatement method

---
+++ internal/evaluator/evaluate_block_statement_test.go
@@ -0,0 +1,55 @@
+package evaluator
+
+import (
+	"io"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/ast"
+	"github.com/Dobefu/DLiteScript/internal/datavalue"
+)
+
+func TestEvaluateBlockStatement(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		input    *ast.BlockStatement
+		expected datavalue.Value
+	}{
+		{
+			input: &ast.BlockStatement{
+				Statements: []ast.ExprNode{
+					&ast.NumberLiteral{Value: "5", StartPos: 0, EndPos: 1},
+				},
+				StartPos: 0,
+				EndPos:   1,
+			},
+			expected: datavalue.Number(5),
+		},
+		{
+			input: &ast.BlockStatement{
+				Statements: []ast.ExprNode{
+					&ast.BreakStatement{Count: 1, StartPos: 0, EndPos: 1},
+				},
+				StartPos: 0,
+				EndPos:   3,
+			},
+			expected: datavalue.Null(),
+		},
+	}
+
+	for _, test := range tests {
+		rawResult, err := NewEvaluator(io.Discard).evaluateBlockStatement(test.input)
+
+		if err != nil {
+			t.Fatalf("error evaluating %s: %s", test.input.Expr(), err.Error())
+		}
+
+		if rawResult.Value.DataType() != test.expected.DataType() {
+			t.Fatalf(
+				"expected \"%s\", got \"%s\"",
+				test.expected.DataType().AsString(),
+				rawResult.Value.DataType().AsString(),
+			)
+		}
+	}
+}
