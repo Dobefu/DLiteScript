3116e548e32d18f4bcc2d07c817015feb567466d
Connor van Spronssen <info@dobefu.com>
2025-08-23 12:35:41 +0200

    test: Add tests for the evaluateNullLiteral method

---
+++ internal/evaluator/evaluate_null_literal_test.go
@@ -0,0 +1,44 @@
+package evaluator
+
+import (
+	"io"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/ast"
+	"github.com/Dobefu/DLiteScript/internal/datavalue"
+)
+
+func TestEvaluateNullLiteral(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		input    ast.ExprNode
+		expected datavalue.Value
+	}{
+		{
+			input:    &ast.NullLiteral{StartPos: 0, EndPos: 0},
+			expected: datavalue.Null(),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.input.Expr(), func(t *testing.T) {
+			t.Parallel()
+
+			result, err := NewEvaluator(io.Discard).Evaluate(test.input)
+
+			if err != nil {
+				t.Fatalf("error evaluating null literal: \"%s\"", err.Error())
+			}
+
+			if result.Value.DataType().AsString() != test.expected.DataType().AsString() {
+				t.Fatalf(
+					"expected \"%v\", got \"%v\" at position %d",
+					test.expected.DataType().AsString(),
+					result.Value.DataType().AsString(),
+					test.input.StartPosition(),
+				)
+			}
+		})
+	}
+}
