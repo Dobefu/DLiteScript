84600403193cd2e2fe89ee32072f4b0da0a3690a
Connor van Spronssen <info@dobefu.com>
2025-08-24 10:38:49 +0200

    test: Add tests for the handleEqualSign method

---
+++ internal/tokenizer/handle_equal_sign_test.go
@@ -0,0 +1,96 @@
+package tokenizer
+
+import (
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/token"
+)
+
+func TestHandleEqualSign(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		input    string
+		expected *token.Token
+	}{
+		{
+			name:  "no extra equal sign",
+			input: "= ",
+			expected: token.NewToken(
+				"=",
+				token.TokenTypeAssign,
+				0,
+				0,
+			),
+		},
+		{
+			name:  "extra equal sign",
+			input: "==",
+			expected: token.NewToken(
+				"==",
+				token.TokenTypeEqual,
+				0,
+				0,
+			),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			tokenizer := NewTokenizer(test.input)
+			_, _ = tokenizer.GetNext()
+			token, err := tokenizer.handleEqualSign(1)
+
+			if err != nil {
+				t.Fatalf("expected no error, got %v", err)
+			}
+
+			if token.Atom != test.expected.Atom {
+				t.Fatalf("expected %s, got %s", test.expected.Atom, token.Atom)
+			}
+
+			if token.TokenType != test.expected.TokenType {
+				t.Fatalf(
+					"expected %T, got %T",
+					test.expected.TokenType,
+					token.TokenType,
+				)
+			}
+		})
+	}
+}
+
+func TestHandleEqualSignErr(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "unexpected end of expression",
+			input:    "",
+			expected: "unexpected end of expression at position 0",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			_, err := NewTokenizer(test.input).handleEqualSign(1)
+
+			if err == nil {
+				t.Fatalf("expected error, got nil")
+			}
+
+			if err.Error() != test.expected {
+				t.Fatalf("expected \"%s\", got \"%s\"", test.expected, err.Error())
+			}
+		})
+	}
+}
