27592dd3138993d4e514e965777cb7adb91c57c2
Connor van Spronssen <info@dobefu.com>
2025-08-24 10:42:20 +0200

    test: Add tests for the handleExclamationSign method

---
+++ internal/tokenizer/handle_exclamation_sign_test.go
@@ -0,0 +1,96 @@
+package tokenizer
+
+import (
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/token"
+)
+
+func TestHandleExclamationSign(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		input    string
+		expected *token.Token
+	}{
+		{
+			name:  "no equal sign after",
+			input: "! ",
+			expected: token.NewToken(
+				"!",
+				token.TokenTypeNot,
+				0,
+				0,
+			),
+		},
+		{
+			name:  "equal sign after exclamation sign",
+			input: "!=",
+			expected: token.NewToken(
+				"!=",
+				token.TokenTypeNotEqual,
+				0,
+				0,
+			),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			tokenizer := NewTokenizer(test.input)
+			_, _ = tokenizer.GetNext()
+			token, err := tokenizer.handleExclamationSign(1)
+
+			if err != nil {
+				t.Fatalf("expected no error, got %v", err)
+			}
+
+			if token.Atom != test.expected.Atom {
+				t.Fatalf("expected %s, got %s", test.expected.Atom, token.Atom)
+			}
+
+			if token.TokenType != test.expected.TokenType {
+				t.Fatalf(
+					"expected %T, got %T",
+					test.expected.TokenType,
+					token.TokenType,
+				)
+			}
+		})
+	}
+}
+
+func TestHandleExclamationSignErr(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "unexpected end of expression",
+			input:    "",
+			expected: "unexpected end of expression at position 0",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			_, err := NewTokenizer(test.input).handleExclamationSign(1)
+
+			if err == nil {
+				t.Fatalf("expected error, got nil")
+			}
+
+			if err.Error() != test.expected {
+				t.Fatalf("expected \"%s\", got \"%s\"", test.expected, err.Error())
+			}
+		})
+	}
+}
