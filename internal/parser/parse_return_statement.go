3c43abae0e6fca27a3f2f014915f2635c46f2a9b
Connor van Spronssen <info@dobefu.com>
2025-08-24 17:59:00 +0200

    feat: Add parsing for return statements

--- internal/parser/parse_return_statement.go
+++ internal/parser/parse_return_statement.go
@@ -1,7 +1,111 @@
 package parser
 
-import "github.com/Dobefu/DLiteScript/internal/ast"
+import (
+	"github.com/Dobefu/DLiteScript/internal/ast"
+	"github.com/Dobefu/DLiteScript/internal/token"
+)
 
 func (p *Parser) parseReturnStatement() (ast.ExprNode, error) {
-	return nil, nil
+	startPos := p.GetCurrentCharPos()
+	nextToken, err := p.PeekNextToken()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if p.isReturnValueTerminator(nextToken) {
+		return &ast.ReturnStatement{
+			Values:    []ast.ExprNode{},
+			NumValues: 0,
+			StartPos:  startPos,
+			EndPos:    p.GetCurrentCharPos(),
+		}, nil
+	}
+
+	firstToken, err := p.GetNextToken()
+
+	if err != nil {
+		return nil, err
+	}
+
+	returnValues, err := p.parseReturnValues(firstToken)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &ast.ReturnStatement{
+		Values:    returnValues,
+		NumValues: len(returnValues),
+		StartPos:  startPos,
+		EndPos:    p.GetCurrentCharPos(),
+	}, nil
+}
+
+func (p *Parser) parseReturnValues(firstToken *token.Token) ([]ast.ExprNode, error) {
+	if firstToken.TokenType == token.TokenTypeComma {
+		return []ast.ExprNode{}, nil
+	}
+
+	firstValue, err := p.parseExpr(firstToken, nil, 0, 0)
+
+	if err != nil {
+		return nil, err
+	}
+
+	returnValues := []ast.ExprNode{firstValue}
+
+	return p.parseAdditionalValues(returnValues)
+}
+
+func (p *Parser) parseAdditionalValues(returnValues []ast.ExprNode) ([]ast.ExprNode, error) {
+	for !p.isEOF {
+		nextToken, err := p.PeekNextToken()
+
+		if err != nil {
+			return nil, err
+		}
+
+		if p.isReturnValueTerminator(nextToken) {
+			break
+		}
+
+		if nextToken.TokenType == token.TokenTypeComma {
+			if err := p.consumeReturnComma(); err != nil {
+				return nil, err
+			}
+
+			continue
+		}
+
+		value, err := p.parseNextReturnValue()
+
+		if err != nil {
+			return nil, err
+		}
+
+		returnValues = append(returnValues, value)
+	}
+
+	return returnValues, nil
+}
+
+func (p *Parser) isReturnValueTerminator(t *token.Token) bool {
+	return t.TokenType == token.TokenTypeNewline || t.TokenType == token.TokenTypeRBrace
+}
+
+func (p *Parser) consumeReturnComma() error {
+	_, err := p.GetNextToken()
+
+	return err
+}
+
+func (p *Parser) parseNextReturnValue() (ast.ExprNode, error) {
+	nextToken, err := p.GetNextToken()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return p.parseExpr(nextToken, nil, 0, 0)
 }
