2c243aea303b572a3f5df1adb0fae6cb29d7b073
Connor van Spronssen <info@dobefu.com>
2025-08-25 06:29:07 +0200

    test: Add tests for the SpreadExpr AST node

---
+++ internal/ast/spread_expr_test.go
@@ -0,0 +1,81 @@
+package ast
+
+import "testing"
+
+func TestSpreadExpr(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name             string
+		input            *SpreadExpr
+		expectedValue    string
+		expectedStartPos int
+		expectedEndPos   int
+		expectedNodes    []string
+	}{
+		{
+			name: "simple",
+			input: &SpreadExpr{
+				Expression: &NumberLiteral{
+					Value:    "1",
+					StartPos: 0,
+					EndPos:   3,
+				},
+				StartPos: 0,
+				EndPos:   3,
+			},
+			expectedValue:    "...1",
+			expectedStartPos: 0,
+			expectedEndPos:   3,
+			expectedNodes:    []string{"...1", "1"},
+		},
+	}
+
+	for _, test := range tests {
+		visitedNodes := []string{}
+
+		test.input.Walk(func(node ExprNode) bool {
+			visitedNodes = append(visitedNodes, node.Expr())
+
+			return true
+		})
+
+		if len(visitedNodes) != len(test.expectedNodes) {
+			t.Fatalf(
+				"Expected %d visited nodes, got %d",
+				len(test.expectedNodes),
+				len(visitedNodes),
+			)
+		}
+
+		for idx, node := range visitedNodes {
+			if node != test.expectedNodes[idx] {
+				t.Fatalf("Expected \"%s\", got \"%s\"", test.expectedNodes[idx], node)
+			}
+		}
+
+		if test.input.Expr() != test.expectedValue {
+			t.Fatalf(
+				"expected '%s', got '%s'",
+				test.expectedValue,
+				test.input.Expr(),
+			)
+		}
+
+		if test.input.StartPosition() != test.expectedStartPos {
+			t.Errorf(
+				"expected pos '%d', got '%d'",
+				test.expectedStartPos,
+				test.input.StartPosition(),
+			)
+		}
+
+		if test.input.EndPosition() != test.expectedEndPos {
+			t.Errorf(
+				"expected pos '%d', got '%d'",
+				test.expectedEndPos,
+				test.input.EndPosition(),
+			)
+		}
+	}
+}
