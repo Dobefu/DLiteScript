0cee092d71c10feb65000e965308cd37686994ef
Connor van Spronssen <info@dobefu.com>
2025-08-25 06:21:54 +0200

    fix: Resolve an issue where spread operators were checked even inside of a number

--- internal/tokenizer/handle_number_or_spread.go
+++ internal/tokenizer/handle_number_or_spread.go
@@ -3,14 +3,16 @@ package tokenizer
 import "github.com/Dobefu/DLiteScript/internal/token"
 
 func (t *Tokenizer) handleNumberOrSpread(next rune, startPos int) (*token.Token, error) {
-	afterNext, err := t.Peek()
+	if next == '.' {
+		afterNext, err := t.Peek()
 
-	if err != nil {
-		return nil, err
-	}
+		if err != nil {
+			return nil, err
+		}
 
-	if afterNext == '.' {
-		return t.parseSpread(startPos)
+		if afterNext == '.' {
+			return t.parseSpread(startPos)
+		}
 	}
 
 	return t.parseNumber(next, startPos)
