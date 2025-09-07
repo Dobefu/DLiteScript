bd98da2e0e293dac61c198bbef6d1aedf099e46a
Connor van Spronssen <info@dobefu.com>
2025-09-07 18:27:45 +0200

    test: Add a test for the GetGlobalFunctions function

---
+++ internal/stdlib/global/global_test.go
@@ -0,0 +1,15 @@
+package global
+
+import (
+	"testing"
+)
+
+func TestGetGlobalFunctions(t *testing.T) {
+	t.Parallel()
+
+	functions := GetGlobalFunctions()
+
+	if len(functions) <= 0 {
+		t.Fatalf("expected at least 1 function, got %d", len(functions))
+	}
+}
