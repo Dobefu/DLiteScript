a7b3b696b703d8b769e71f695610f39094824f9b
Connor van Spronssen <info@dobefu.com>
2025-09-07 15:55:57 +0200

    feat: Add logic to parse a single dot token

--- internal/tokenizer/handle_number_or_spread.go
+++ internal/tokenizer/handle_number_or_spread.go
@@ -13,6 +13,14 @@ func (t *Tokenizer) handleNumberOrSpread(next rune, startPos int) (*token.Token,
 		if afterNext == '.' {
 			return t.parseSpread(startPos)
 		}
+
+		switch afterNext {
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			return t.parseNumber('.', startPos)
+
+		default:
+			return token.NewToken(".", token.TokenTypeDot, startPos, t.expIdx), nil
+		}
 	}
 
 	return t.parseNumber(next, startPos)
