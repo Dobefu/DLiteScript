8ad5e60bb05b368ab0f2f9e275fb1877f497b749
Connor van Spronssen <info@dobefu.com>
2025-09-10 13:36:44 +0200

    test: Add tests for the parseConstantDeclaration method

---
+++ internal/parser/parse_constant_declaration_test.go
@@ -0,0 +1,138 @@
+package parser
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/errorutil"
+	"github.com/Dobefu/DLiteScript/internal/token"
+)
+
+func TestParseConstantDeclaration(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		input    []*token.Token
+		expected string
+	}{
+		{
+			name: "constant declaration",
+			input: []*token.Token{
+				{Atom: "const", TokenType: token.TokenTypeConst},
+				{Atom: "x", TokenType: token.TokenTypeIdentifier},
+				{Atom: "number", TokenType: token.TokenTypeTypeNumber},
+				{Atom: "=", TokenType: token.TokenTypeAssign},
+				{Atom: "1", TokenType: token.TokenTypeNumber},
+			},
+			expected: "const x number = 1",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			p := NewParser(test.input)
+			result, err := p.Parse()
+
+			if err != nil {
+				t.Fatalf("expected no error, got: %s", err.Error())
+			}
+
+			if result.Expr() != test.expected {
+				t.Fatalf("expected \"%s\", got \"%s\"", test.expected, result.Expr())
+			}
+		})
+	}
+}
+
+func TestParseConstantDeclarationErr(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		input    []*token.Token
+		expected string
+	}{
+		{
+			name: "unexpected EOF after const",
+			input: []*token.Token{
+				{Atom: "const", TokenType: token.TokenTypeConst},
+			},
+			expected: fmt.Sprintf(
+				"%s at position 1",
+				errorutil.ErrorMsgUnexpectedEOF,
+			),
+		},
+		{
+			name: "unexpected EOF after declaration",
+			input: []*token.Token{
+				{Atom: "const", TokenType: token.TokenTypeConst},
+				{Atom: "x", TokenType: token.TokenTypeIdentifier},
+				{Atom: "number", TokenType: token.TokenTypeTypeNumber},
+			},
+			expected: fmt.Sprintf(
+				"%s at position 3",
+				errorutil.ErrorMsgUnexpectedEOF,
+			),
+		},
+		{
+			name: "no assignment operator after declaration",
+			input: []*token.Token{
+				{Atom: "const", TokenType: token.TokenTypeConst},
+				{Atom: "x", TokenType: token.TokenTypeIdentifier},
+				{Atom: "number", TokenType: token.TokenTypeTypeNumber},
+				{Atom: "\n", TokenType: token.TokenTypeNewline},
+			},
+			expected: fmt.Sprintf(
+				"%s at position 4",
+				fmt.Sprintf(errorutil.ErrorMsgConstantDeclarationWithNoValue, "x"),
+			),
+		},
+		{
+			name: "unexpected EOF after value",
+			input: []*token.Token{
+				{Atom: "const", TokenType: token.TokenTypeConst},
+				{Atom: "x", TokenType: token.TokenTypeIdentifier},
+				{Atom: "number", TokenType: token.TokenTypeTypeNumber},
+				{Atom: "=", TokenType: token.TokenTypeAssign},
+			},
+			expected: fmt.Sprintf(
+				"%s at position 4",
+				errorutil.ErrorMsgUnexpectedEOF,
+			),
+		},
+		{
+			name: "unexpected value",
+			input: []*token.Token{
+				{Atom: "const", TokenType: token.TokenTypeConst},
+				{Atom: "x", TokenType: token.TokenTypeIdentifier},
+				{Atom: "number", TokenType: token.TokenTypeTypeNumber},
+				{Atom: "=", TokenType: token.TokenTypeAssign},
+				{Atom: "}", TokenType: token.TokenTypeRBrace},
+			},
+			expected: fmt.Sprintf(
+				"%s at position 5",
+				fmt.Sprintf(errorutil.ErrorMsgUnexpectedToken, "}"),
+			),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			p := NewParser(test.input)
+			_, err := p.Parse()
+
+			if err == nil {
+				t.Fatalf("expected error, got nil")
+			}
+
+			if err.Error() != test.expected {
+				t.Fatalf("expected \"%s\", got \"%s\"", test.expected, err.Error())
+			}
+		})
+	}
+}
