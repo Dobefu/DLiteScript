20b369b36ca69e34d4d71c064164d27ea23ccd64
Connor van Spronssen <info@dobefu.com>
2025-09-14 10:47:24 +0200

    test: Add additional tests for the `math.round` function

--- internal/stdlib/math/round_test.go
+++ internal/stdlib/math/round_test.go
@@ -3,37 +3,60 @@ package math
 import (
 	"testing"
 
-	"github.com/Dobefu/DLiteScript/internal/datatype"
 	"github.com/Dobefu/DLiteScript/internal/datavalue"
-	"github.com/Dobefu/DLiteScript/internal/function"
 )
 
 func TestGetRoundFunction(t *testing.T) {
 	t.Parallel()
 
+	tests := []struct {
+		name     string
+		input    datavalue.Value
+		expected datavalue.Value
+	}{
+		{
+			name:     "positive number (1.5)",
+			input:    datavalue.Number(1.5),
+			expected: datavalue.Number(2),
+		},
+		{
+			name:     "positive number (1.2)",
+			input:    datavalue.Number(1.2),
+			expected: datavalue.Number(1),
+		},
+		{
+			name:     "positive number (1)",
+			input:    datavalue.Number(1),
+			expected: datavalue.Number(1),
+		},
+		{
+			name:     "negative number (-1.5)",
+			input:    datavalue.Number(-1.5),
+			expected: datavalue.Number(-2),
+		},
+	}
+
 	functions := GetMathFunctions()
 
-	if _, ok := functions["round"]; !ok {
-		t.Fatalf("expected round function, got %v", functions)
-	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
 
-	roundFunc := functions["round"]
+			roundFunc, hasFunction := functions["round"]
 
-	if roundFunc.FunctionType != function.FunctionTypeFixed {
-		t.Fatalf("expected fixed function, got %v", roundFunc.FunctionType)
-	}
+			if !hasFunction {
+				t.Fatalf("could not find round function")
+			}
 
-	if roundFunc.Parameters[0].Type != datatype.DataTypeNumber {
-		t.Fatalf("expected number argument, got %v", roundFunc.Parameters[0].Type)
-	}
+			result, err := roundFunc.Handler(nil, []datavalue.Value{test.input})
 
-	result, err := roundFunc.Handler(nil, []datavalue.Value{datavalue.Number(1.5)})
+			if err != nil {
+				t.Fatalf("expected no error, got: \"%s\"", err.Error())
+			}
 
-	if err != nil {
-		t.Fatalf("expected no error, got %v", err)
-	}
-
-	if result.Num != 2 {
-		t.Fatalf("expected 2, got %v", result.Num)
+			if result.Num != test.expected.Num {
+				t.Fatalf("expected %f, got %f", test.expected.Num, result.Num)
+			}
+		})
 	}
 }
