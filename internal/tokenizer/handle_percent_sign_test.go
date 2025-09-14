220ce5b3534086e624481f7ba61b38235aa6142d
Connor van Spronssen <info@dobefu.com>
2025-09-14 11:14:46 +0200

    test: Add tests for the handlePercentSign method

---
+++ internal/tokenizer/handle_percent_sign_test.go
@@ -0,0 +1,94 @@
+package tokenizer
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/errorutil"
+	"github.com/Dobefu/DLiteScript/internal/token"
+)
+
+func TestHandlePercentSign(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		input    string
+		expected *token.Token
+	}{
+		{
+			name:  "no relevant sign after",
+			input: "% ",
+			expected: token.NewToken(
+				"%",
+				token.TokenTypeOperationMod,
+				0,
+				0,
+			),
+		},
+		{
+			name:  "equal sign after percent sign",
+			input: "%=",
+			expected: token.NewToken(
+				"%=",
+				token.TokenTypeOperationModAssign,
+				0,
+				0,
+			),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			tokenizer := NewTokenizer(test.input)
+			_, _ = tokenizer.GetNext()
+			token, err := tokenizer.handlePercentSign(1)
+
+			if err != nil {
+				t.Fatalf("expected no error, got %v", err)
+			}
+
+			if token.Atom != test.expected.Atom {
+				t.Fatalf("expected %s, got %s", test.expected.Atom, token.Atom)
+			}
+		})
+	}
+}
+
+func TestHandlePercentSignErr(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:  "unexpected end of expression",
+			input: "",
+			expected: fmt.Sprintf(
+				"%s: %s at position 0",
+				errorutil.StageTokenize.String(),
+				errorutil.ErrorMsgUnexpectedEOF,
+			),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			_, err := NewTokenizer(test.input).handlePercentSign(1)
+
+			if err == nil {
+				t.Fatalf("expected error, got nil")
+			}
+
+			if err.Error() != test.expected {
+				t.Fatalf("expected %s, got %s", test.expected, err.Error())
+			}
+		})
+	}
+}
