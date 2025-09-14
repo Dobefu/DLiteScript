05c913821c9593d30e5138a130510e30ac7f6aa3
Connor van Spronssen <info@dobefu.com>
2025-09-14 14:59:19 +0200

    test: Add additional tests for the IfStatement AST node

--- internal/ast/if_statement_test.go
+++ internal/ast/if_statement_test.go
@@ -8,13 +8,16 @@ func TestIfStatement(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
+		name             string
 		input            ExprNode
 		expectedValue    string
 		expectedStartPos int
 		expectedEndPos   int
 		expectedNodes    []string
+		continueOn       string
 	}{
 		{
+			name: "if statement without else",
 			input: &IfStatement{
 				Condition: &BoolLiteral{Value: "true", StartPos: 0, EndPos: 1},
 				ThenBlock: &BlockStatement{
@@ -40,8 +43,10 @@ func TestIfStatement(t *testing.T) {
 				"1",
 				"1",
 			},
+			continueOn: "",
 		},
 		{
+			name: "if statement with else",
 			input: &IfStatement{
 				Condition: &BoolLiteral{Value: "true", StartPos: 0, EndPos: 1},
 				ThenBlock: &BlockStatement{
@@ -77,50 +82,171 @@ func TestIfStatement(t *testing.T) {
 				"2",
 				"2",
 			},
+			continueOn: "",
+		},
+		{
+			name: "if statement with nil condition",
+			input: &IfStatement{
+				Condition: nil,
+				ThenBlock: &BlockStatement{
+					Statements: []ExprNode{
+						&NumberLiteral{Value: "1", StartPos: 2, EndPos: 3},
+					},
+					StartPos: 2,
+					EndPos:   3,
+				},
+				StartPos:  0,
+				EndPos:    5,
+				ElseBlock: nil,
+			},
+			expectedValue:    "",
+			expectedStartPos: 0,
+			expectedEndPos:   5,
+			expectedNodes: []string{
+				"",
+				"(1)",
+				"(1)",
+				"1",
+				"1",
+			},
+			continueOn: "",
+		},
+		{
+			name: "walk early return after if statement",
+			input: &IfStatement{
+				Condition: &BoolLiteral{Value: "false", StartPos: 0, EndPos: 1},
+				ThenBlock: &BlockStatement{
+					Statements: []ExprNode{
+						&NumberLiteral{Value: "1", StartPos: 2, EndPos: 3},
+					},
+					StartPos: 2,
+					EndPos:   3,
+				},
+				StartPos:  0,
+				EndPos:    5,
+				ElseBlock: nil,
+			},
+			expectedValue:    "if false { (1) }",
+			expectedStartPos: 0,
+			expectedEndPos:   5,
+			expectedNodes: []string{
+				"if false { (1) }",
+			},
+			continueOn: "if false { (1) }",
+		},
+		{
+			name: "walk early return after condition",
+			input: &IfStatement{
+				Condition: &BoolLiteral{Value: "true", StartPos: 0, EndPos: 1},
+				ThenBlock: &BlockStatement{
+					Statements: []ExprNode{
+						&NumberLiteral{Value: "1", StartPos: 2, EndPos: 3},
+					},
+					StartPos: 2,
+					EndPos:   3,
+				},
+				StartPos:  0,
+				EndPos:    5,
+				ElseBlock: nil,
+			},
+			expectedValue:    "if true { (1) }",
+			expectedStartPos: 0,
+			expectedEndPos:   5,
+			expectedNodes: []string{
+				"if true { (1) }",
+				"true",
+			},
+			continueOn: "true",
+		},
+		{
+			name: "walk early return after then block",
+			input: &IfStatement{
+				Condition: &BoolLiteral{Value: "true", StartPos: 0, EndPos: 1},
+				ThenBlock: &BlockStatement{
+					Statements: []ExprNode{
+						&NumberLiteral{Value: "1", StartPos: 2, EndPos: 3},
+					},
+					StartPos: 2,
+					EndPos:   3,
+				},
+				StartPos:  0,
+				EndPos:    5,
+				ElseBlock: nil,
+			},
+			expectedValue:    "if true { (1) }",
+			expectedStartPos: 0,
+			expectedEndPos:   5,
+			expectedNodes: []string{
+				"if true { (1) }",
+				"true",
+				"true",
+				"(1)",
+			},
+			continueOn: "(1)",
+		},
+		{
+			name: "walk early return after else block",
+			input: &IfStatement{
+				Condition: &BoolLiteral{Value: "true", StartPos: 0, EndPos: 1},
+				ThenBlock: &BlockStatement{
+					Statements: []ExprNode{
+						&NumberLiteral{Value: "1", StartPos: 2, EndPos: 3},
+					},
+					StartPos: 2,
+					EndPos:   3,
+				},
+				StartPos: 0,
+				EndPos:   5,
+				ElseBlock: &BlockStatement{
+					Statements: []ExprNode{
+						&NumberLiteral{Value: "2", StartPos: 4, EndPos: 5},
+					},
+					StartPos: 4,
+					EndPos:   5,
+				},
+			},
+			expectedValue:    "if true { (1) } else { (2) }",
+			expectedStartPos: 0,
+			expectedEndPos:   5,
+			expectedNodes: []string{
+				"if true { (1) } else { (2) }",
+				"true",
+				"true",
+				"(1)",
+				"(1)",
+				"1",
+				"1",
+				"(2)",
+			},
+			continueOn: "(2)",
 		},
 	}
 
 	for _, test := range tests {
-		visitedNodes := []string{}
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
 
-		test.input.Walk(func(node ExprNode) bool {
-			visitedNodes = append(visitedNodes, node.Expr())
-
-			return true
-		})
-
-		if len(visitedNodes) != len(test.expectedNodes) {
-			t.Fatalf(
-				"Expected %d visited nodes, got %d",
-				len(test.expectedNodes),
-				len(visitedNodes),
-			)
-		}
-
-		for idx, node := range visitedNodes {
-			if node != test.expectedNodes[idx] {
-				t.Fatalf("Expected \"%s\", got \"%s\"", test.expectedNodes[idx], node)
+			if test.input.Expr() != test.expectedValue {
+				t.Errorf("expected '%s', got '%s'", test.expectedValue, test.input.Expr())
 			}
-		}
 
-		if test.input.Expr() != test.expectedValue {
-			t.Errorf("expected '%s', got '%s'", test.expectedValue, test.input.Expr())
-		}
+			if test.input.StartPosition() != test.expectedStartPos {
+				t.Errorf(
+					"expected pos '%d', got '%d'",
+					test.expectedStartPos,
+					test.input.StartPosition(),
+				)
+			}
 
-		if test.input.StartPosition() != test.expectedStartPos {
-			t.Errorf(
-				"expected pos '%d', got '%d'",
-				test.expectedStartPos,
-				test.input.StartPosition(),
-			)
-		}
+			if test.input.EndPosition() != test.expectedEndPos {
+				t.Errorf(
+					"expected pos '%d', got '%d'",
+					test.expectedEndPos,
+					test.input.EndPosition(),
+				)
+			}
 
-		if test.input.EndPosition() != test.expectedEndPos {
-			t.Errorf(
-				"expected pos '%d', got '%d'",
-				test.expectedEndPos,
-				test.input.EndPosition(),
-			)
-		}
+			WalkUntil(t, test.input, test.expectedNodes, test.continueOn)
+		})
 	}
 }
