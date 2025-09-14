d8488c26e0bd052367daa60260f406fc4a386af4
Connor van Spronssen <info@dobefu.com>
2025-09-14 15:00:19 +0200

    test: Add additional tests for the IndexAssignmentStatement AST node

--- internal/ast/index_assignment_statement_test.go
+++ internal/ast/index_assignment_statement_test.go
@@ -7,43 +7,124 @@ import (
 func TestIndexAssignmentStatement(t *testing.T) {
 	t.Parallel()
 
-	statement := &IndexAssignmentStatement{
-		Array:    &Identifier{Value: "array", StartPos: 0, EndPos: 5},
-		Index:    &NumberLiteral{Value: "0", StartPos: 0, EndPos: 1},
-		Right:    &NumberLiteral{Value: "1", StartPos: 0, EndPos: 1},
-		StartPos: 0,
-		EndPos:   5,
+	tests := []struct {
+		name             string
+		input            ExprNode
+		expectedValue    string
+		expectedStartPos int
+		expectedEndPos   int
+		expectedNodes    []string
+		continueOn       string
+	}{
+		{
+			name: "index assignment statement",
+			input: &IndexAssignmentStatement{
+				Array:    &Identifier{Value: "array", StartPos: 0, EndPos: 5},
+				Index:    &NumberLiteral{Value: "0", StartPos: 0, EndPos: 1},
+				Right:    &NumberLiteral{Value: "1", StartPos: 0, EndPos: 1},
+				StartPos: 0,
+				EndPos:   5,
+			},
+			expectedValue:    "array[0] = 1",
+			expectedStartPos: 0,
+			expectedEndPos:   5,
+			expectedNodes:    []string{"array[0] = 1", "array", "array", "0", "0", "1", "1"},
+			continueOn:       "",
+		},
+		{
+			name: "index assignment with nil array",
+			input: &IndexAssignmentStatement{
+				Array:    nil,
+				Index:    &NumberLiteral{Value: "0", StartPos: 0, EndPos: 1},
+				Right:    &NumberLiteral{Value: "1", StartPos: 0, EndPos: 1},
+				StartPos: 0,
+				EndPos:   5,
+			},
+			expectedValue:    "",
+			expectedStartPos: 0,
+			expectedEndPos:   5,
+			expectedNodes:    []string{"", "0", "0", "1", "1"},
+			continueOn:       "",
+		},
+		{
+			name: "index assignment with nil index",
+			input: &IndexAssignmentStatement{
+				Array:    &Identifier{Value: "array", StartPos: 0, EndPos: 5},
+				Index:    nil,
+				Right:    &NumberLiteral{Value: "1", StartPos: 0, EndPos: 1},
+				StartPos: 0,
+				EndPos:   5,
+			},
+			expectedValue:    "",
+			expectedStartPos: 0,
+			expectedEndPos:   5,
+			expectedNodes:    []string{"", "array", "array", "1", "1"},
+			continueOn:       "",
+		},
+		{
+			name: "walk early return after index assignment",
+			input: &IndexAssignmentStatement{
+				Array:    &Identifier{Value: "arr", StartPos: 0, EndPos: 3},
+				Index:    &NumberLiteral{Value: "2", StartPos: 0, EndPos: 1},
+				Right:    &NumberLiteral{Value: "3", StartPos: 0, EndPos: 1},
+				StartPos: 0,
+				EndPos:   5,
+			},
+			expectedValue:    "arr[2] = 3",
+			expectedStartPos: 0,
+			expectedEndPos:   5,
+			expectedNodes:    []string{"arr[2] = 3"},
+			continueOn:       "arr[2] = 3",
+		},
+		{
+			name: "walk early return after array",
+			input: &IndexAssignmentStatement{
+				Array:    &Identifier{Value: "list", StartPos: 0, EndPos: 4},
+				Index:    &NumberLiteral{Value: "1", StartPos: 0, EndPos: 1},
+				Right:    &NumberLiteral{Value: "2", StartPos: 0, EndPos: 1},
+				StartPos: 0,
+				EndPos:   5,
+			},
+			expectedValue:    "list[1] = 2",
+			expectedStartPos: 0,
+			expectedEndPos:   5,
+			expectedNodes:    []string{"list[1] = 2", "list"},
+			continueOn:       "list",
+		},
+		{
+			name: "walk early return after index",
+			input: &IndexAssignmentStatement{
+				Array:    &Identifier{Value: "data", StartPos: 0, EndPos: 4},
+				Index:    &NumberLiteral{Value: "5", StartPos: 0, EndPos: 1},
+				Right:    &NumberLiteral{Value: "6", StartPos: 0, EndPos: 1},
+				StartPos: 0,
+				EndPos:   5,
+			},
+			expectedValue:    "data[5] = 6",
+			expectedStartPos: 0,
+			expectedEndPos:   5,
+			expectedNodes:    []string{"data[5] = 6", "data", "data", "5"},
+			continueOn:       "5",
+		},
 	}
 
-	expectedNodes := []string{"array[0] = 1", "array", "array", "0", "0", "1", "1"}
-	expectedValue := "array[0] = 1"
-	visitedNodes := []string{}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
 
-	statement.Walk(func(node ExprNode) bool {
-		visitedNodes = append(visitedNodes, node.Expr())
+			if test.input.Expr() != test.expectedValue {
+				t.Errorf("expected '%s', got '%s'", test.expectedValue, test.input.Expr())
+			}
 
-		return true
-	})
+			if test.input.StartPosition() != test.expectedStartPos {
+				t.Errorf("expected pos '%d', got '%d'", test.expectedStartPos, test.input.StartPosition())
+			}
 
-	if len(visitedNodes) != len(expectedNodes) {
-		t.Fatalf("Expected %d visited nodes, got %d", len(expectedNodes), len(visitedNodes))
-	}
+			if test.input.EndPosition() != test.expectedEndPos {
+				t.Errorf("expected pos '%d', got '%d'", test.expectedEndPos, test.input.EndPosition())
+			}
 
-	for idx, node := range visitedNodes {
-		if node != expectedNodes[idx] {
-			t.Fatalf("Expected \"%s\", got \"%s\"", expectedNodes[idx], node)
-		}
-	}
-
-	if statement.Expr() != expectedValue {
-		t.Fatalf("Expected \"%s\", got \"%s\"", expectedValue, statement.Expr())
-	}
-
-	if statement.StartPosition() != 0 {
-		t.Fatalf("Expected start position to be 0, got %d", statement.StartPosition())
-	}
-
-	if statement.EndPosition() != 5 {
-		t.Fatalf("Expected end position to be 5, got %d", statement.EndPosition())
+			WalkUntil(t, test.input, test.expectedNodes, test.continueOn)
+		})
 	}
 }
