294088d2984b61db99d9607ad706a1e0b904f8f9
Connor van Spronssen <info@dobefu.com>
2025-09-18 13:30:59 +0200

    test: Add tests for the `strings.len` function

---
+++ internal/stdlib/strings/len_test.go
@@ -0,0 +1,63 @@
+package strings
+
+import (
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/datavalue"
+)
+
+func TestGetLenFunction(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		input    datavalue.Value
+		args     datavalue.Value
+		expected datavalue.Value
+	}{
+		{
+			name:     "empty string",
+			input:    datavalue.String(""),
+			args:     datavalue.Number(1),
+			expected: datavalue.Number(0),
+		},
+		{
+			name:     "simple string",
+			input:    datavalue.String("test"),
+			args:     datavalue.Number(1),
+			expected: datavalue.Number(4),
+		},
+	}
+
+	lenFunc := getLenFunction()
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			result, err := lenFunc.Handler(
+				nil,
+				[]datavalue.Value{test.input, test.args},
+			)
+
+			if err != nil {
+				t.Fatalf("expected no error, got: \"%s\"", err.Error())
+			}
+
+			expectedNumber, _ := test.expected.AsNumber()
+			number, err := result.AsNumber()
+
+			if err != nil {
+				t.Fatalf("expected no error, got: \"%s\"", err.Error())
+			}
+
+			if number != expectedNumber {
+				t.Fatalf(
+					"expected %g, got: %g",
+					expectedNumber,
+					number,
+				)
+			}
+		})
+	}
+}
