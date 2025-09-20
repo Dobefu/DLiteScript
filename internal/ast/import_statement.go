e7a0eafc2fbf642078d76586ef5b7723249609b5
Connor van Spronssen <info@dobefu.com>
2025-09-20 19:30:43 +0200

    chore: Add an ImportStatement AST node

---
+++ internal/ast/import_statement.go
@@ -0,0 +1,32 @@
+package ast
+
+import (
+	"fmt"
+)
+
+// ImportStatement represents an import statement in the AST.
+type ImportStatement struct {
+	Path     *StringLiteral
+	StartPos int
+	EndPos   int
+}
+
+// Expr returns the expression of the import statement.
+func (i *ImportStatement) Expr() string {
+	return fmt.Sprintf("import %s", i.Path.Expr())
+}
+
+// StartPosition returns the start position of the import statement.
+func (i *ImportStatement) StartPosition() int {
+	return i.StartPos
+}
+
+// EndPosition returns the end position of the import statement.
+func (i *ImportStatement) EndPosition() int {
+	return i.EndPos
+}
+
+// Walk walks the import statement.
+func (i *ImportStatement) Walk(fn func(node ExprNode) bool) {
+	fn(i)
+}
