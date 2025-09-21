627ca52a8a2166208287fd8e5213b9a5283b07d1
Connor van Spronssen <info@dobefu.com>
2025-09-21 18:14:25 +0200

    test: Add a test for the formatComment method

---
+++ internal/formatter/format_comment_test.go
@@ -0,0 +1,41 @@
+package formatter
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/ast"
+)
+
+func TestFormatComment(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name      string
+		input     *ast.CommentLiteral
+		formatter *Formatter
+		depth     int
+		expected  string
+	}{
+		{
+			name:      "comment literal",
+			input:     &ast.CommentLiteral{Value: "Comment", StartPos: 0, EndPos: 1},
+			formatter: &Formatter{indentSize: 2, indentChar: " "},
+			depth:     0,
+			expected:  "Comment\n",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			builder := &strings.Builder{}
+			test.formatter.formatComment(test.input, builder, test.depth)
+
+			if builder.String() != test.expected {
+				t.Errorf("expected '%s', got '%s'", test.expected, builder.String())
+			}
+		})
+	}
+}
