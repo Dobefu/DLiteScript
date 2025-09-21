d5f213e359912e67123570459089a204a2bfc021
Connor van Spronssen <info@dobefu.com>
2025-09-21 13:45:39 +0200

    test: Add an additional test for the evaluateVariableDeclaration method

--- internal/evaluator/evaluate_variable_declaration_test.go
+++ internal/evaluator/evaluate_variable_declaration_test.go
@@ -7,9 +7,70 @@ import (
 	"testing"
 
 	"github.com/Dobefu/DLiteScript/internal/ast"
+	"github.com/Dobefu/DLiteScript/internal/datatype"
 	"github.com/Dobefu/DLiteScript/internal/datavalue"
 	"github.com/Dobefu/DLiteScript/internal/errorutil"
 )
+
+func TestEvaluateVariableDeclaration(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		input    *ast.VariableDeclaration
+		expected datavalue.Value
+	}{
+		{
+			name: "variable declaration with value",
+			input: &ast.VariableDeclaration{
+				Name:     "x",
+				Type:     datatype.DataTypeNumber.AsString(),
+				Value:    &ast.NumberLiteral{Value: "1", StartPos: 0, EndPos: 1},
+				StartPos: 0,
+				EndPos:   1,
+			},
+			expected: datavalue.Number(1),
+		},
+		{
+			name: "variable declaration without value",
+			input: &ast.VariableDeclaration{
+				Name:     "x",
+				Type:     datatype.DataTypeNumber.AsString(),
+				Value:    nil,
+				StartPos: 0,
+				EndPos:   1,
+			},
+			expected: datavalue.Number(0),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			ev := NewEvaluator(io.Discard)
+			_, err := ev.evaluateVariableDeclaration(test.input)
+
+			if err != nil {
+				t.Fatalf("expected no error, got \"%s\"", err.Error())
+			}
+
+			variable, hasVariable := ev.outerScope[test.input.Name]
+
+			if !hasVariable {
+				t.Fatalf("expected variable \"%s\" to be stored in scope", test.input.Name)
+			}
+
+			if variable.GetValue().DataType != test.expected.DataType {
+				t.Fatalf(
+					"expected \"%s\", got \"%s\"",
+					test.expected.DataType.AsString(),
+					variable.GetValue().DataType.AsString(),
+				)
+			}
+		})
+	}
+}
 
 func TestEvaluateVariableDeclarationErr(t *testing.T) {
 	t.Parallel()
@@ -52,17 +113,17 @@ func TestEvaluateVariableDeclarationErr(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
+
+			_, err := NewEvaluator(io.Discard).evaluateVariableDeclaration(test.input)
+
+			if err == nil {
+				t.Fatalf("expected error evaluating %s, got nil", test.input.Expr())
+			}
+
+			if errors.Unwrap(err).Error() != test.expected {
+				t.Fatalf("expected \"%s\", got \"%s\"", test.expected, errors.Unwrap(err).Error())
+			}
 		})
-
-		_, err := NewEvaluator(io.Discard).evaluateVariableDeclaration(test.input)
-
-		if err == nil {
-			t.Fatalf("expected error evaluating %s, got nil", test.input.Expr())
-		}
-
-		if errors.Unwrap(err).Error() != test.expected {
-			t.Fatalf("expected \"%s\", got \"%s\"", test.expected, errors.Unwrap(err).Error())
-		}
 	}
 }
 
@@ -114,16 +175,16 @@ func TestGetZeroValueForType(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
+
+			ev := NewEvaluator(io.Discard)
+
+			if !ev.getZeroValueForType(test.input).Equals(test.expected) {
+				t.Fatalf(
+					"expected \"%s\", got \"%s\"",
+					test.expected.ToString(),
+					ev.getZeroValueForType(test.input).ToString(),
+				)
+			}
 		})
-
-		ev := NewEvaluator(io.Discard)
-
-		if !ev.getZeroValueForType(test.input).Equals(test.expected) {
-			t.Fatalf(
-				"expected \"%s\", got \"%s\"",
-				test.expected.ToString(),
-				ev.getZeroValueForType(test.input).ToString(),
-			)
-		}
 	}
 }
