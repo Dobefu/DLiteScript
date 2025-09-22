e62455706ae3402392fd36280598f69a59300972
Connor van Spronssen <info@dobefu.com>
2025-09-22 17:17:51 +0200

    test: Add a test for the formatVariableDeclaration method

---
+++ internal/formatter/format_variable_declaration_test.go
@@ -0,0 +1,51 @@
+package formatter
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/ast"
+)
+
+func TestFormatVariableDeclaration(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name      string
+		input     *ast.VariableDeclaration
+		formatter *Formatter
+		depth     int
+		expected  string
+	}{
+		{
+			name: "variable declaration",
+			input: &ast.VariableDeclaration{
+				Name: "x",
+				Type: "int",
+				Value: &ast.NumberLiteral{
+					Value:    "1",
+					StartPos: 0,
+					EndPos:   1,
+				},
+				StartPos: 0,
+				EndPos:   1,
+			},
+			formatter: &Formatter{indentSize: 2, indentChar: " "},
+			depth:     0,
+			expected:  "var x int = 1\n",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			builder := &strings.Builder{}
+			test.formatter.formatVariableDeclaration(test.input, builder, test.depth)
+
+			if builder.String() != test.expected {
+				t.Errorf("expected '%s', got '%s'", test.expected, builder.String())
+			}
+		})
+	}
+}
