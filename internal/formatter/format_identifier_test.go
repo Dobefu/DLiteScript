bd10d8a1f6f9592ff50ace828e967d9b2f394d35
Connor van Spronssen <info@dobefu.com>
2025-09-22 17:04:56 +0200

    test: Add a test for the formatIdentifier method

---
+++ internal/formatter/format_identifier_test.go
@@ -0,0 +1,41 @@
+package formatter
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/ast"
+)
+
+func TestFormatIdentifier(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name      string
+		input     *ast.Identifier
+		formatter *Formatter
+		depth     int
+		expected  string
+	}{
+		{
+			name:      "identifier",
+			input:     &ast.Identifier{Value: "x", StartPos: 0, EndPos: 1},
+			formatter: &Formatter{indentSize: 2, indentChar: " "},
+			depth:     0,
+			expected:  "x\n",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			builder := &strings.Builder{}
+			test.formatter.formatIdentifier(test.input, builder, test.depth)
+
+			if builder.String() != test.expected {
+				t.Errorf("expected '%s', got '%s'", test.expected, builder.String())
+			}
+		})
+	}
+}
