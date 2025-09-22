61b40d6ce677c204bf7265cbc23a66a2f3cfab80
Connor van Spronssen <info@dobefu.com>
2025-09-22 17:09:33 +0200

    test: Add a test for the formatAssignmentStatement method

---
+++ internal/formatter/format_assignment_statement_test.go
@@ -0,0 +1,50 @@
+package formatter
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/ast"
+)
+
+func TestFormatAssignmentStatement(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name      string
+		input     *ast.AssignmentStatement
+		formatter *Formatter
+		depth     int
+		expected  string
+	}{
+		{
+			name: "assignment statement",
+			input: &ast.AssignmentStatement{
+				Left: &ast.Identifier{Value: "x", StartPos: 0, EndPos: 1},
+				Right: &ast.NumberLiteral{
+					Value:    "1",
+					StartPos: 0,
+					EndPos:   1,
+				},
+				StartPos: 0,
+				EndPos:   1,
+			},
+			formatter: &Formatter{indentSize: 2, indentChar: " "},
+			depth:     0,
+			expected:  "x = 1\n",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			builder := &strings.Builder{}
+			test.formatter.formatAssignmentStatement(test.input, builder, test.depth)
+
+			if builder.String() != test.expected {
+				t.Errorf("expected '%s', got '%s'", test.expected, builder.String())
+			}
+		})
+	}
+}
