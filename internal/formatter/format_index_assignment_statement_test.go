c525eb3546fec0689b7f26deb479b2f9618906e6
Connor van Spronssen <info@dobefu.com>
2025-09-22 17:14:19 +0200

    test: Add a test for the formatIndexAssignmentStatement method

---
+++ internal/formatter/format_index_assignment_statement_test.go
@@ -0,0 +1,47 @@
+package formatter
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/ast"
+)
+
+func TestFormatIndexAssignmentStatement(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name      string
+		input     *ast.IndexAssignmentStatement
+		formatter *Formatter
+		depth     int
+		expected  string
+	}{
+		{
+			name: "index assignment statement",
+			input: &ast.IndexAssignmentStatement{
+				Array:    &ast.Identifier{Value: "array", StartPos: 0, EndPos: 5},
+				Index:    &ast.NumberLiteral{Value: "1", StartPos: 0, EndPos: 1},
+				Right:    &ast.NumberLiteral{Value: "1", StartPos: 0, EndPos: 1},
+				StartPos: 0,
+				EndPos:   5,
+			},
+			formatter: &Formatter{indentSize: 2, indentChar: " "},
+			depth:     0,
+			expected:  "array[1] = 1\n",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			builder := &strings.Builder{}
+			test.formatter.formatIndexAssignmentStatement(test.input, builder, test.depth)
+
+			if builder.String() != test.expected {
+				t.Errorf("expected '%s', got '%s'", test.expected, builder.String())
+			}
+		})
+	}
+}
