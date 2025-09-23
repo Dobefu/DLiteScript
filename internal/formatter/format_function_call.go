779eb0a66068cde3c60d20be201d4d3b7bde7f5b
Connor van Spronssen <info@dobefu.com>
2025-09-23 08:25:28 +0200

    feat: Implement logic for the formatFunctionCall method

--- internal/formatter/format_function_call.go
+++ internal/formatter/format_function_call.go
@@ -12,6 +12,29 @@ func (f *Formatter) formatFunctionCall(
 	depth int,
 ) {
 	f.addWhitespace(result, depth)
-	result.WriteString(node.Expr())
-	result.WriteString("\n")
+
+	functionName := node.FunctionName
+
+	if node.Namespace != "" {
+		functionName = node.Namespace + "." + node.FunctionName
+	}
+
+	result.WriteString(functionName)
+	result.WriteString("(")
+
+	if len(node.Arguments) > 0 {
+		for i, arg := range node.Arguments {
+			if arg == nil {
+				continue
+			}
+
+			if i > 0 {
+				result.WriteString(", ")
+			}
+
+			result.WriteString(arg.Expr())
+		}
+	}
+
+	result.WriteString(")\n")
 }
