a5feaf57785538d525f354b9169561c26e43270d
Connor van Spronssen <info@dobefu.com>
2025-09-23 06:03:54 +0200

    test: Add a test for the formatBlockStatement method

---
+++ internal/formatter/format_block_statement_test.go
@@ -0,0 +1,47 @@
+package formatter
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/ast"
+)
+
+func TestFormatBlockStatement(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name      string
+		input     *ast.BlockStatement
+		formatter *Formatter
+		depth     int
+		expected  string
+	}{
+		{
+			name: "block statement",
+			input: &ast.BlockStatement{
+				Statements: []ast.ExprNode{
+					&ast.NumberLiteral{Value: "1", StartPos: 0, EndPos: 1},
+				},
+				StartPos: 0,
+				EndPos:   1,
+			},
+			formatter: &Formatter{indentSize: 2, indentChar: " "},
+			depth:     0,
+			expected:  "{\n  1\n}\n",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			builder := &strings.Builder{}
+			test.formatter.formatNode(test.input, builder, test.depth)
+
+			if builder.String() != test.expected {
+				t.Errorf("expected \"%s\", got \"%s\"", test.expected, builder.String())
+			}
+		})
+	}
+}
