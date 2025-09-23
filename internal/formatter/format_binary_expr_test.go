d6341e611485fe15cdaa1cb0c74f96671ddfcb66
Connor van Spronssen <info@dobefu.com>
2025-09-23 05:55:46 +0200

    test: Add a test for the formatBinaryExpr method

---
+++ internal/formatter/format_binary_expr_test.go
@@ -0,0 +1,51 @@
+package formatter
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/ast"
+	"github.com/Dobefu/DLiteScript/internal/token"
+)
+
+func TestFormatBinaryExpr(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name      string
+		input     *ast.BinaryExpr
+		formatter *Formatter
+		expected  string
+	}{
+		{
+			name: "binary expression",
+			input: &ast.BinaryExpr{
+				Left:  &ast.NumberLiteral{Value: "1", StartPos: 0, EndPos: 1},
+				Right: &ast.NumberLiteral{Value: "1", StartPos: 2, EndPos: 3},
+				Operator: token.Token{
+					Atom:      "+",
+					TokenType: token.TokenTypeOperationAdd,
+					StartPos:  0,
+					EndPos:    0,
+				},
+				StartPos: 0,
+				EndPos:   0,
+			},
+			formatter: &Formatter{indentSize: 2, indentChar: " "},
+			expected:  "1 + 1\n",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			builder := &strings.Builder{}
+			test.formatter.formatNode(test.input, builder, 0)
+
+			if builder.String() != test.expected {
+				t.Errorf("expected '%s', got '%s'", test.expected, builder.String())
+			}
+		})
+	}
+}
