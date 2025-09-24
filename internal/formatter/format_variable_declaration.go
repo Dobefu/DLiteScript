730badd4b6f12f7ca0449b743a14903f135e59fa
Connor van Spronssen <info@dobefu.com>
2025-09-24 19:42:15 +0200

    feat: Add line wrapping for array variable declarations

--- internal/formatter/format_variable_declaration.go
+++ internal/formatter/format_variable_declaration.go
@@ -1,6 +1,7 @@
 package formatter
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/Dobefu/DLiteScript/internal/ast"
@@ -12,6 +13,48 @@ func (f *Formatter) formatVariableDeclaration(
 	depth int,
 ) {
 	f.addWhitespace(result, depth)
-	result.WriteString(node.Expr())
+
+	if node.Value == nil {
+		fmt.Fprintf(result, "var %s %s\n", node.Name, node.Type)
+
+		return
+	}
+
+	varDeclarationPrefix := fmt.Sprintf("var %s %s = ", node.Name, node.Type)
+	totalLineLength := node.Range.End.Offset - node.Range.Start.Offset
+
+	arrayLiteral, isArrayLiteral := node.Value.(*ast.ArrayLiteral)
+
+	if isArrayLiteral && totalLineLength > f.maxLineLength {
+		result.WriteString(varDeclarationPrefix)
+		result.WriteString("[\n")
+
+		for i, value := range arrayLiteral.Values {
+			if value == nil {
+				continue
+			}
+
+			f.addWhitespace(result, depth+1)
+			result.WriteString(value.Expr())
+
+			if i < len(arrayLiteral.Values)-1 {
+				result.WriteString(",")
+			}
+
+			result.WriteString("\n")
+		}
+
+		f.addWhitespace(result, depth)
+		result.WriteString("]\n")
+
+		return
+	}
+
+	fmt.Fprintf(result, "var %s %s = ", node.Name, node.Type)
+
+	var valueBuilder strings.Builder
+	f.formatNode(node.Value, &valueBuilder, 0)
+	valueStr := strings.TrimSuffix(valueBuilder.String(), "\n")
+	result.WriteString(valueStr)
 	result.WriteString("\n")
 }
