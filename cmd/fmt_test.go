93e7445ad0c4c415ebce3478a404db7a04baec8f
Connor van Spronssen <info@dobefu.com>
2025-09-28 11:51:51 +0200

    test: Add additional tests for the `fmt` command

--- cmd/fmt_test.go
+++ cmd/fmt_test.go
@@ -58,6 +58,14 @@ func TestFmtCmdErr(t *testing.T) {
 			name:  "invalid file path",
 			input: "bogus",
 		},
+		{
+			name:  "syntax error",
+			input: "./testfiles/syntax_error.dl",
+		},
+		{
+			name:  "parse error",
+			input: "./testfiles/parse_error.dl",
+		},
 	}
 
 	for _, test := range tests {
