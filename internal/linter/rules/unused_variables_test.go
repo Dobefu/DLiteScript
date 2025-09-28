5fe6a954690deaaa581d1e6708ab86af9bbe2324
Connor van Spronssen <info@dobefu.com>
2025-09-28 09:43:02 +0200

    test: Add tests for the UnusedVariables struct

---
+++ internal/linter/rules/unused_variables_test.go
@@ -0,0 +1,313 @@
+package rules
+
+import (
+	"io"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/ast"
+	"github.com/Dobefu/DLiteScript/internal/linter/reporter"
+	"github.com/Dobefu/DLiteScript/internal/token"
+)
+
+func TestUnusedVariables(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		input    ast.ExprNode
+		expected []*reporter.Issue
+	}{
+		{
+			name: "no unused variables",
+			input: &ast.BlockStatement{
+				Statements: []ast.ExprNode{},
+				Range: ast.Range{
+					Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+					End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+				},
+			},
+			expected: []*reporter.Issue{},
+		},
+		{
+			name: "unused variable",
+			input: &ast.BlockStatement{
+				Statements: []ast.ExprNode{
+					&ast.VariableDeclaration{
+						Name: "x",
+						Type: "number",
+						Value: &ast.NumberLiteral{
+							Value: "1",
+							Range: ast.Range{
+								Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+								End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+							},
+						},
+						Range: ast.Range{
+							Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+							End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+						},
+					},
+				},
+				Range: ast.Range{
+					Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+					End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+				},
+			},
+			expected: []*reporter.Issue{
+				{
+					Rule:    "unused-variables",
+					Message: "variable 'x' is declared but never used",
+					Range: ast.Range{
+						Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+						End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+					},
+					Severity: reporter.SeverityWarning,
+				},
+			},
+		},
+		{
+			name: "unused constant",
+			input: &ast.BlockStatement{
+				Statements: []ast.ExprNode{
+					&ast.ConstantDeclaration{
+						Name: "x",
+						Type: "number",
+						Value: &ast.NumberLiteral{
+							Value: "1",
+							Range: ast.Range{
+								Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+								End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+							},
+						},
+						Range: ast.Range{
+							Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+							End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+						},
+					},
+				},
+				Range: ast.Range{
+					Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+					End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+				},
+			},
+			expected: []*reporter.Issue{
+				{
+					Rule:    "unused-variables",
+					Message: "constant 'x' is declared but never used",
+					Range: ast.Range{
+						Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+						End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+					},
+					Severity: reporter.SeverityWarning,
+				},
+			},
+		},
+		{
+			name: "constant used in identifier",
+			input: &ast.BlockStatement{
+				Statements: []ast.ExprNode{
+					&ast.ConstantDeclaration{
+						Name: "x",
+						Type: "number",
+						Value: &ast.NumberLiteral{
+							Value: "1",
+							Range: ast.Range{
+								Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+								End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+							},
+						},
+						Range: ast.Range{
+							Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+							End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+						},
+					},
+					&ast.Identifier{
+						Value: "x",
+						Range: ast.Range{
+							Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+							End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+						},
+					},
+				},
+				Range: ast.Range{
+					Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+					End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+				},
+			},
+			expected: []*reporter.Issue{},
+		},
+		{
+			name: "variable used in assignment statement",
+			input: &ast.BlockStatement{
+				Statements: []ast.ExprNode{
+					&ast.VariableDeclaration{
+						Name: "x",
+						Type: "number",
+						Value: &ast.NumberLiteral{
+							Value: "1",
+							Range: ast.Range{
+								Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+								End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+							},
+						},
+						Range: ast.Range{
+							Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+							End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+						},
+					},
+					&ast.AssignmentStatement{
+						Left: &ast.Identifier{
+							Value: "x",
+							Range: ast.Range{
+								Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+								End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+							},
+						},
+						Right: &ast.NumberLiteral{
+							Value: "2",
+							Range: ast.Range{
+								Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+								End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+							},
+						},
+						Range: ast.Range{
+							Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+							End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+						},
+					},
+				},
+				Range: ast.Range{
+					Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+					End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+				},
+			},
+			expected: []*reporter.Issue{},
+		},
+		{
+			name: "variable used in shorthand assignment",
+			input: &ast.BlockStatement{
+				Statements: []ast.ExprNode{
+					&ast.VariableDeclaration{
+						Name: "x",
+						Type: "number",
+						Value: &ast.NumberLiteral{
+							Value: "1",
+							Range: ast.Range{
+								Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+								End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+							},
+						},
+						Range: ast.Range{
+							Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+							End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+						},
+					},
+					&ast.ShorthandAssignmentExpr{
+						Left: &ast.Identifier{
+							Value: "x",
+							Range: ast.Range{
+								Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+								End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+							},
+						},
+						Operator: *token.NewToken("+=", token.TokenTypeOperationAddAssign, 0, 1),
+						Right: &ast.NumberLiteral{
+							Value: "1",
+							Range: ast.Range{
+								Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+								End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+							},
+						},
+						Range: ast.Range{
+							Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+							End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+						},
+					},
+				},
+				Range: ast.Range{
+					Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+					End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+				},
+			},
+			expected: []*reporter.Issue{},
+		},
+		{
+			name: "shorthand assignment with nil left",
+			input: &ast.BlockStatement{
+				Statements: []ast.ExprNode{
+					&ast.VariableDeclaration{
+						Name: "x",
+						Type: "number",
+						Value: &ast.NumberLiteral{
+							Value: "1",
+							Range: ast.Range{
+								Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+								End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+							},
+						},
+						Range: ast.Range{
+							Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+							End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+						},
+					},
+					&ast.ShorthandAssignmentExpr{
+						Left:     nil,
+						Operator: *token.NewToken("+=", token.TokenTypeOperationAddAssign, 0, 1),
+						Right: &ast.NumberLiteral{
+							Value: "1",
+							Range: ast.Range{
+								Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+								End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+							},
+						},
+						Range: ast.Range{
+							Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+							End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+						},
+					},
+				},
+				Range: ast.Range{
+					Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+					End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+				},
+			},
+			expected: []*reporter.Issue{
+				{
+					Rule:    "unused-variables",
+					Message: "variable 'x' is declared but never used",
+					Range: ast.Range{
+						Start: ast.Position{Offset: 0, Line: 0, Column: 0},
+						End:   ast.Position{Offset: 0, Line: 0, Column: 0},
+					},
+					Severity: reporter.SeverityWarning,
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			rule := NewUnusedVariables(reporter.NewReporter(io.Discard))
+
+			if len(rule.Name()) == 0 {
+				t.Fatalf("expected name, got none")
+			}
+
+			if len(rule.Description()) == 0 {
+				t.Fatalf("expected description, got none")
+			}
+
+			rule.Analyze(test.input)
+
+			if len(rule.reporter.GetIssues()) != len(test.expected) {
+				t.Fatalf(
+					"expected %d issues, got %d",
+					len(test.expected),
+					len(rule.reporter.GetIssues()),
+				)
+			}
+		})
+	}
+}
