7fcc010c9a47d4b2617555802558b2ea428e7b79
Connor van Spronssen <info@dobefu.com>
2025-10-22 08:22:35 +0200

    feat(docs): Add playground logic to disable tabbing in the textarea

---
+++ docs/assets/playground/js/ide.js
@@ -0,0 +1,43 @@
+(() => {
+  "use strict";
+
+  /** @type {NodeListOf<HTMLTextAreaElement>} */
+  const ides = document.querySelectorAll(".ide");
+
+  for (const ide of ides) {
+    let wasEscapePressed = false;
+
+    /**
+     * @param {KeyboardEvent} e
+     */
+    ide.addEventListener("keydown", (e) => {
+      console.log(wasEscapePressed);
+      switch (e.key) {
+        case "Tab":
+          if (!wasEscapePressed) {
+            e.preventDefault();
+          }
+
+          wasEscapePressed = false;
+
+          break;
+
+        case "Escape":
+          wasEscapePressed = true;
+
+          break;
+
+        case "Control":
+        case "Alt":
+        case "Meta":
+        case "Shift":
+          break;
+
+        default:
+          wasEscapePressed = false;
+
+          break;
+      }
+    });
+  }
+})();
