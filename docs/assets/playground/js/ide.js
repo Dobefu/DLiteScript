a4e677370e6cf31da9270acd8056860464dfd072
Connor van Spronssen <info@dobefu.com>
2025-10-24 11:39:53 +0200

    feat(docs): Add logic to indent or outdent code

--- docs/assets/playground/js/ide.js
+++ docs/assets/playground/js/ide.js
@@ -15,6 +15,37 @@
         case "Tab":
           if (!wasEscapePressed) {
             e.preventDefault();
+            const lines = ide.value.split("\n");
+            let selectionStart = ide.selectionStart;
+            let selectionEnd = ide.selectionEnd;
+
+            const selectedLines = getLinesFromSelection(
+              lines,
+              selectionStart,
+              selectionEnd,
+            );
+
+            if (e.shiftKey) {
+              for (const lineIdx of selectedLines) {
+                const lineLen = lines[lineIdx].length;
+                lines[lineIdx] = lines[lineIdx].replace(/^\s{1,2}/, "");
+
+                selectionEnd -= lineLen - lines[lineIdx].length;
+              }
+            } else {
+              for (const lineIdx of selectedLines) {
+                lines[lineIdx] = `  ${lines[lineIdx]}`;
+
+                selectionEnd += 2;
+              }
+            }
+
+            ide.value = lines.join("\n");
+            ide.dispatchEvent(new Event("input"));
+            ide.dispatchEvent(new Event("change"));
+
+            ide.selectionStart = selectionStart;
+            ide.selectionEnd = selectionEnd;
           }
 
           wasEscapePressed = false;
@@ -38,5 +69,31 @@
           break;
       }
     });
+  }
+
+  /**
+   * @param {string[]} lines
+   * @param {number} start
+   * @param {number} end
+   */
+  function getLinesFromSelection(lines, start, end) {
+    /** @type {number[]} */
+    const selectedLines = [];
+
+    let lineIdx = 0;
+    let charIdx = 0;
+
+    do {
+      charIdx += lines[lineIdx].length;
+
+      if (charIdx >= start) {
+        selectedLines.push(lineIdx);
+      }
+
+      charIdx++;
+      lineIdx++;
+    } while (charIdx <= end);
+
+    return selectedLines;
   }
 })();
