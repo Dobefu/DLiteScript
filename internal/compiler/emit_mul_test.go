186fa0779fb8ee12b4ab3df457f30e888ee591fb
Connor van Spronssen <info@dobefu.com>
2025-11-03 07:29:33 +0100

    test: Add tests for the emitMul method

---
+++ internal/compiler/emit_mul_test.go
@@ -0,0 +1,56 @@
+package compiler
+
+import (
+	"reflect"
+	"testing"
+
+	vm "github.com/Dobefu/vee-em"
+)
+
+func TestEmitMul(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		dest     byte
+		src1     byte
+		src2     byte
+		expected []byte
+	}{
+		{
+			name:     "mul (1 * 1)",
+			dest:     0,
+			src1:     0,
+			src2:     0,
+			expected: []byte{byte(vm.OpcodeMul), 0, 0, 0},
+		},
+		{
+			name:     "mul (2 * 2)",
+			dest:     0,
+			src1:     1,
+			src2:     1,
+			expected: []byte{byte(vm.OpcodeMul), 0, 1, 1},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			c := NewCompiler()
+			err := c.emitMul(test.dest, test.src1, test.src2)
+
+			if err != nil {
+				t.Fatalf("Expected no error, got: \"%s\"", err.Error())
+			}
+
+			if !reflect.DeepEqual(c.bytecode, test.expected) {
+				t.Fatalf(
+					"expected bytecode to be %v, got %v",
+					test.expected,
+					c.bytecode,
+				)
+			}
+		})
+	}
+}
