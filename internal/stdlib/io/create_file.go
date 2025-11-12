b8b1e032cc772582a6a24c16f8259534971b3550
sharkmu <csonka.samuel@gmail.com>
2025-11-12 20:08:01 +0100

    feat(io): complete createFile function

---
+++ internal/stdlib/io/create_file.go
@@ -0,0 +1,68 @@
+package io
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Dobefu/DLiteScript/internal/datatype"
+	"github.com/Dobefu/DLiteScript/internal/datavalue"
+	"github.com/Dobefu/DLiteScript/internal/function"
+)
+
+func getCreateFileFunction() function.Info {
+	return function.MakeFunction(
+		function.Documentation{
+			Name:        "createFile",
+			Description: "Creates an empty file.",
+			Since:       "v0.2.0",
+			DeprecationInfo: function.DeprecationInfo{
+				IsDeprecated: false,
+				Description:  "",
+				Version:      "",
+			},
+			Examples: []string{
+				fmt.Sprintf(`%s.createFile("awesome.txt") // creates an empty file called "awesome.txt" or something`, packageName),
+			},
+		},
+		packageName,
+		function.FunctionTypeFixed,
+		[]function.ArgInfo{
+			{
+				Type:        datatype.DataTypeString,
+				Name:        "path",
+				Description: "The path for the file that should be created.",
+			},
+		},
+		[]function.ArgInfo{
+			{
+				Type:        datatype.DataTypeString,
+				Name:        "error",
+				Description: "An error, if the file cannot be created.",
+			},
+		},
+		true,
+		func(_ function.EvaluatorInterface, args []datavalue.Value) datavalue.Value {
+			path, _ := args[0].AsString()
+
+			exists := getExistsFunction()
+			result, err := exists.Handler(
+				nil,
+				[]datavalue.Value{
+					datavalue.String(path),
+				},
+			)
+			if err != nil {
+				return datavalue.Error(err)
+			}
+
+			if !result.Bool {
+				err := os.WriteFile(path, []byte(""), 0644)
+				if err != nil {
+					return datavalue.Error(err)
+				}
+				return datavalue.Error(nil)
+			}
+			return datavalue.Error(fmt.Errorf("File %v already exists", path))
+		},
+	)
+}
---
+++ internal/stdlib/io/create_file_test.go
@@ -0,0 +1,50 @@
+package io
+
+import (
+	"os"
+	"testing"
+
+	"github.com/Dobefu/DLiteScript/internal/datavalue"
+)
+
+func TestGetCreateFileFunction(t *testing.T) {
+	t.Parallel()
+
+	fileName := "data.txt"
+	if err := os.WriteFile(fileName, []byte(""), 0644); err != nil {
+		t.Fatalf("unable to create / write to file %s: %v", fileName, err)
+	}
+
+	getCreateFileFunc := getCreateFileFunction()
+
+	result, err := getCreateFileFunc.Handler(
+		nil,
+		[]datavalue.Value{
+			datavalue.String(fileName),
+		},
+	)
+	if err != nil {
+		t.Fatalf("expected no error from handler, got: %v", err)
+	}
+
+	if result.Error == nil {
+		t.Fatal("expected error from func, but got nil")
+	}
+
+	os.Remove(fileName)
+
+	getCreateFileFunc = getCreateFileFunction()
+
+	result, err = getCreateFileFunc.Handler(
+		nil,
+		[]datavalue.Value{
+			datavalue.String(fileName),
+		},
+	)
+
+	if result.Error != nil {
+		t.Fatalf("expected no error from func, but got: %v", result.Error)
+	}
+
+	os.Remove(fileName)
+}
