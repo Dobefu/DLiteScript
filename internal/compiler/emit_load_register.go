134ab9bec6af996cb80fb3506b3ff808ebcf0dfd
Connor van Spronssen <info@dobefu.com>
2025-12-07 16:27:42 +0100

    chore: Group some consecutive parameters of the same type

--- internal/compiler/emit_load_register.go
+++ internal/compiler/emit_load_register.go
@@ -4,7 +4,7 @@ import (
 	vm "github.com/Dobefu/vee-em"
 )
 
-func (c *Compiler) emitLoadRegister(dest byte, src byte) error {
+func (c *Compiler) emitLoadRegister(dest, src byte) error {
 	c.bytecode = append(c.bytecode, byte(vm.OpcodeLoadRegister))
 	c.bytecode = append(c.bytecode, dest, src)
 
